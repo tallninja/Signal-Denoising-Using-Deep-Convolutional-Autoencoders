c60170ce4da3dde59f67a72c8fe4d32208cf6f40
tallninja <ernestwambua2@gmail.com>
2023-11-27 09:24:06 +0300

    Run model inference

--- audio_utils.py
+++ audio_utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy import interpolate
+import torchaudio
+import torch
 
 
 def resample(original, old_rate, new_rate):
@@ -11,3 +13,19 @@ def resample(original, old_rate, new_rate):
         interpolator = interpolate.interp1d(old_time, original.T)
         return interpolator(new_time).T
     return original
+
+
+def load_audio_file(file_path='sample.wav', max_len=165000):
+    waveform, _ = torchaudio.load(file_path)
+    waveform = waveform.numpy()
+    current_len = waveform.shape[1]
+    output = np.zeros((1, max_len), dtype='float32')
+    output[0, -current_len:] = waveform[0, :max_len]
+    return torch.from_numpy(output)
+
+
+def save_audio_file(dest_path, audio_data, sample_rate=48000, bit_precision=16):
+    audio_data = np.reshape(audio_data, (1, -1))
+    audio_data = torch.from_numpy(audio_data)
+    torchaudio.save(dest_path, audio_data, sample_rate,
+                    bits_per_sample=bit_precision)
